304dd0ec04ab94c5b60d4fd473e42b62594d6330
chengyb <xitmen@163.com>
2016-11-07 17:09:54 +0800

    新增搜索

---
+++ data/knowledge.js
@@ -0,0 +1,518 @@
+window.knowledge = [
+  {
+    "id":1,
+    "title": "机器人的定义",
+    "html": "1.html",
+    "type": 'knowledge'
+  },
+  {
+    "id":2,
+    "title": "机器人三原则",
+    "html": "2.html",
+    "type": 'knowledge'
+  },
+  {
+    "id":3,
+    "title": "机器人具有特性",
+    "html": "3.html",
+    "type": 'knowledge'
+  },
+  {
+    "id":4,
+    "title": "什么是工业机器人？",
+    "html": "4.html",
+    "type": 'knowledge'
+  },
+  {
+    "id":5,
+    "title": "机器人的发展",
+    "html": "5.html",
+    "type": 'knowledge'
+  },
+  {
+    "id":6,
+    "title": "机器人的主要技术参数",
+    "html": "6.html",
+    "type": 'knowledge'
+  },
+  {
+    "id":7,
+    "title": "工业机器人分类",
+    "html": "7.html",
+    "type": 'knowledge'
+  },
+  {
+    "id":8,
+    "title": "中国的机器人是如何分类？",
+    "html": "8.html",
+    "type": 'knowledge'
+  },
+  {
+    "id":9,
+    "title": "工业机器人作业特点",
+    "html": "9.html",
+    "type": 'knowledge'
+  },
+  {
+    "id":10,
+    "title": "工业机器人的特点",
+    "html": "10.html",
+    "type": 'knowledge'
+  },
+  {
+    "id":11,
+    "title": "机器人组成",
+    "html": "11.html",
+    "type": 'knowledge'
+  },
+  {
+    "id":12,
+    "title": "工业机器人末端操作器一般有哪几类？",
+    "html": "12.html",
+    "type": 'knowledge'
+  },
+  {
+    "id":13,
+    "title": "机器人采用履带方式优点是什么？",
+    "html": "13.html",
+    "type": 'knowledge'
+  },
+  {
+    "id":14,
+    "title": "驱动系统采用液压驱动优缺点",
+    "html": "14.html",
+    "type": 'knowledge'
+  },
+  {
+    "id":15,
+    "title": "驱动系统采用气压驱动的优缺点",
+    "html": "15.html",
+    "type": 'knowledge'
+  },
+  {
+    "id":16,
+    "title": "工业机器人的触觉",
+    "html": "16.html",
+    "type": 'knowledge'
+  },
+  {
+    "id":17,
+    "title": "工业机器人的手爪有哪些分类吗？",
+    "html": "17.html",
+    "type": 'knowledge'
+  },
+  {
+    "id":18,
+    "title": "采用机器人二维视觉系统是否能进行多产品混合生产？",
+    "html": "18.html",
+    "type": 'knowledge'
+  },
+  {
+    "id":19,
+    "title": "自动上下料机器人可以提高工效吗？",
+    "html": "19.html",
+    "type": 'knowledge'
+  },
+  {
+    "id":20,
+    "title": "请问焊接自动化的工业机器人有哪些要求？",
+    "html": "20.html",
+    "type": 'knowledge'
+  },
+  {
+    "id":21,
+    "title": "介绍一下机器人对无定位工件的搬运？",
+    "html": "21.html",
+    "type": 'knowledge'
+  },
+  {
+    "id":22,
+    "title": "什么叫焊接机器人？",
+    "html": "22.html",
+    "type": 'knowledge'
+  },
+  {
+    "id":23,
+    "title": "焊接机器人工作站是怎样组成？",
+    "html": "23.html",
+    "type": 'knowledge'
+  },
+  {
+    "id":24,
+    "title": "为什么会用焊接机器人？",
+    "html": "24.html",
+    "type": 'knowledge'
+  },
+  {
+    "id":25,
+    "title": "请问焊接机器人的有哪些优点？",
+    "html": "25.html",
+    "type": 'knowledge'
+  },
+  {
+    "id":26,
+    "title": "什么是切削机器人？",
+    "html": "26.html",
+    "type": 'knowledge'
+  },
+  {
+    "id":27,
+    "title": "请问机加工机器人有哪些应用？",
+    "html": "27.html",
+    "type": 'knowledge'
+  },
+  {
+    "id":28,
+    "title": "弧焊机器人系统设计由哪些部分组成？",
+    "html": "28.html",
+    "type": 'knowledge'
+  },
+  {
+    "id":29,
+    "title": "什么叫去毛刺机器人？",
+    "html": "29.html",
+    "type": 'knowledge'
+  },
+  {
+    "id":30,
+    "title": "什么是智能工业机器人？",
+    "html": "30.html",
+    "type": 'knowledge'
+  },
+  {
+    "id":31,
+    "title": "瑞典ABB适用热锻上下料机器人的型号有哪些？",
+    "html": "31.html",
+    "type": 'knowledge'
+  },
+  {
+    "id":32,
+    "title": "请问机器人的装配自动线具有哪些优势？",
+    "html": "32.html",
+    "type": 'knowledge'
+  },
+  {
+    "id":33,
+    "title": "什么是机器视觉系统？",
+    "html": "33.html",
+    "type": 'knowledge'
+  },
+  {
+    "id":34,
+    "title": "数控车床上下料机器人是怎样作业的？",
+    "html": "34.html",
+    "type": 'knowledge'
+  },
+  {
+    "id":35,
+    "title": "有没有全自动轮毂冲压锻压生产线上下料机器人？",
+    "html": "35.html",
+    "type": 'knowledge'
+  },
+  {
+    "id":36,
+    "title": "请问有没有用在汽车缓冲器铸件加工的铸加工上下料机器人？",
+    "html": "36.html",
+    "type": 'knowledge'
+  },
+  {
+    "id":37,
+    "title": "机器人可以用在哪些行业？",
+    "html": "37.html",
+    "type": 'knowledge'
+  },
+  {
+    "id":38,
+    "title": "安川组合机器人主要用在哪些方面？",
+    "html": "38.html",
+    "type": 'knowledge'
+  },
+  {
+    "id":39,
+    "title": "请问去毛刺机器人的工作原理是什么？",
+    "html": "39.html",
+    "type": 'knowledge'
+  },
+  {
+    "id":40,
+    "title": "采用离线编程可以带来哪些效益？",
+    "html": "40.html",
+    "type": 'knowledge'
+  },
+  {
+    "id":41,
+    "title": "机器人需要做哪些日常保养呢？",
+    "html": "41.html",
+    "type": 'knowledge'
+  },
+  {
+    "id":42,
+    "title": "请问喷涂机器人的主要优点是什么？",
+    "html": "42.html",
+    "type": 'knowledge'
+  },
+  {
+    "id":43,
+    "title": "请问离线编程系统有什么特点？",
+    "html": "43.html",
+    "type": 'knowledge'
+  },
+  {
+    "id":44,
+    "title": "机器人编程中IML语言是什么？",
+    "html": "44.html",
+    "type": 'knowledge'
+  },
+  {
+    "id":45,
+    "title": "机器人焊接系统中的夹具及部件快换的设计和快换方式？",
+    "html": "45.html",
+    "type": 'knowledge'
+  },
+  {
+    "id":46,
+    "title": "铸件精整加工机器人与手动加工相比，有什么显著的优点？",
+    "html": "46.html",
+    "type": 'knowledge'
+  },
+  {
+    "id":47,
+    "title": "焊接系统中整根夹具梁快换的设计和快换方式是怎样的？",
+    "html": "47.html",
+    "type": 'knowledge'
+  },
+  {
+    "id":48,
+    "title": "什么是装配力控制？",
+    "html": "48.html",
+    "type": 'knowledge'
+  },
+  {
+    "id":49,
+    "title": "发动机装配线要保证发动机的装配技术条件，要怎么做？",
+    "html": "49.html",
+    "type": 'knowledge'
+  },
+  {
+    "id":50,
+    "title": "机加工机器人的是如何组成的？",
+    "html": "50.html",
+    "type": 'knowledge'
+  },
+  {
+    "id":51,
+    "title": "两台压机串联冲压的机器人自动送料系统有什么好处？",
+    "html": "51.html",
+    "type": 'knowledge'
+  },
+  {
+    "id":52,
+    "title": "请问机器人需要做原点复归？",
+    "html": "52.html",
+    "type": 'knowledge'
+  },
+  {
+    "id":53,
+    "title": "是不是机器人适合在任何环境下使用呢？",
+    "html": "53.html",
+    "type": 'knowledge'
+  },
+  {
+    "id":54,
+    "title": "请问安川机器人编程指令表IL是什么意思？",
+    "html": "54.html",
+    "type": 'knowledge'
+  },
+  {
+    "id":55,
+    "title": "怎样防止操作机器人的事故？",
+    "html": "55.html",
+    "type": 'knowledge'
+  },
+  {
+    "id":56,
+    "title": "机器人爪手主要是用在哪些方面的？",
+    "html": "56.html",
+    "type": 'knowledge'
+  },
+  {
+    "id":57,
+    "title": "请问机器人无夹具定位工件的搬运系统有什么优点？",
+    "html": "57.html",
+    "type": 'knowledge'
+  },
+  {
+    "id":58,
+    "title": "在汽车制造业上，为什么会用焊接机器人？",
+    "html": "58.html",
+    "type": 'knowledge'
+  },
+  {
+    "id":59,
+    "title": "上下料机器人是由哪些部件集成的，可以完成哪些工作？",
+    "html": "59.html",
+    "type": 'knowledge'
+  },
+  {
+    "id":60,
+    "title": "请问机加工机器人能应用于哪些方面？",
+    "html": "60.html",
+    "type": 'knowledge'
+  },
+  {
+    "id":61,
+    "title": "请问喷涂机器人可以应用在哪些领域？",
+    "html": "61.html",
+    "type": 'knowledge'
+  },
+  {
+    "id":62,
+    "title": "机器人系统装箱与垛码的优势主要体现在哪些地方？",
+    "html": "62.html",
+    "type": 'knowledge'
+  },
+  {
+    "id":63,
+    "title": "机器人自动焊接工作站可以用在哪些行业？",
+    "html": "63.html",
+    "type": 'knowledge'
+  },
+  {
+    "id":64,
+    "title": "请问上下料机器人搬运系统可以用在哪些场合？",
+    "html": "64.html",
+    "type": 'knowledge'
+  },
+  {
+    "id":65,
+    "title": "请问有没有为工艺品雕刻而制造的机器人？",
+    "html": "65.html",
+    "type": 'knowledge'
+  },
+  {
+    "id":66,
+    "title": "ABB RobotStudio的主要用途是什么？",
+    "html": "66.html",
+    "type": 'knowledge'
+  },
+  {
+    "id":67,
+    "title": "什么叫工业机器人集成？",
+    "html": "67.html",
+    "type": 'knowledge'
+  },
+  {
+    "id":68,
+    "title": "变速箱修理后，对变速箱内的油作检查的方法是什么？",
+    "html": "68.html",
+    "type": 'knowledge'
+  },
+  {
+    "id":69,
+    "title": "如果是日常保养机器人的话要做哪些事情呢？",
+    "html": "69.html",
+    "type": 'knowledge'
+  },
+  {
+    "id":70,
+    "title": "怎样检查安川机器人动力电缆的保护壳？",
+    "html": "70.html",
+    "type": 'knowledge'
+  },
+  {
+    "id":71,
+    "title": "我安川机器人的变速箱寿命是多少？",
+    "html": "71.html",
+    "type": 'knowledge'
+  },
+  {
+    "id":72,
+    "title": "在操作工业机器人时要注意哪些问题，以应对突发情况？",
+    "html": "72.html",
+    "type": 'knowledge'
+  },
+  {
+    "id":73,
+    "title": "要更换机器人控制器主板上的电池，应该怎么操作？",
+    "html": "73.html",
+    "type": 'knowledge'
+  },
+  {
+    "id":74,
+    "title": "安川机器人要多久保养一次？",
+    "html": "74.html",
+    "type": 'knowledge'
+  },
+  {
+    "id":75,
+    "title": "怎样对机器人的刹车进行检查？",
+    "html": "75.html",
+    "type": 'knowledge'
+  },
+  {
+    "id":76,
+    "title": "安川机器人是怎样更换平衡块轴承润滑油的？",
+    "html": "76.html",
+    "type": 'knowledge'
+  },
+  {
+    "id":77,
+    "title": "工业机器人应用场合",
+    "html": "77.html",
+    "type": 'knowledge'
+  },
+  {
+    "id":78,
+    "title": "工业机器人研究方向",
+    "html": "78.html",
+    "type": 'knowledge'
+  },
+  {
+    "id":79,
+    "title": "机器人评价指标",
+    "html": "79.html",
+    "type": 'knowledge'
+  },
+  {
+    "id":80,
+    "title": "工业机器人控制系统硬件结构",
+    "html": "80.html",
+    "type": 'knowledge'
+  },
+  {
+    "id":81,
+    "title": "工业机器人控制系统体系结构",
+    "html": "81.html",
+    "type": 'knowledge'
+  },
+  {
+    "id":82,
+    "title": "控制软件开发环境",
+    "html": "82.html",
+    "type": 'knowledge'
+  },
+  {
+    "id":83,
+    "title": "机器人伺服通信总线技术",
+    "html": "83.html",
+    "type": 'knowledge'
+  },
+  {
+    "id":84,
+    "title": "工业机器人进行作业时，操作人员应该注意的几个要点",
+    "html": "84.html",
+    "type": 'knowledge'
+  },
+  {
+    "id":85,
+    "title": "工业机器人常用技术及发",
+    "html": "85.html",
+    "type": 'knowledge'
+  },
+  {
+    "id":86,
+    "title": "用行业市场发展",
+    "html": "86.html",
+    "type": 'knowledge'
+  }
+]
---
+++ data/video.js
@@ -0,0 +1,37 @@
+window.video = [
+  {
+    "id":129,
+    "title": "编程篇 ABB 第七节 动作模式-线性运动",
+    "img": "video01.jpg",
+    "url": "http://200028503.vod.myqcloud.com/200028503_a6ab7f429f0f11e694a34983ad9d2cb1.f30.mp4",
+    "type": 'school'
+  },
+  {
+    "id":130,
+    "title": "编程篇 YASKAWA 第十节 关节坐标系下的轴运动",
+    "img": "video02.jpg",
+    "url": "http://200028503.vod.myqcloud.com/200028503_0acf07f09f1011e694a34983ad9d2cb1.f30.mp4",
+    "type": 'school'
+  },
+  {
+    "id":131,
+    "title": "编程篇 ABB 第六节 动作模式-单轴运动",
+    "img": "video03.jpg",
+    "url": "http://200028503.vod.myqcloud.com/200028503_3fd14dde9f0e11e694a34983ad9d2cb1.f30.mp4",
+    "type": 'school'
+  },
+  {
+    "id":132,
+    "title": "编程篇 YAMAHA 第八节 常用动作指令",
+    "img": "video04.jpg",
+    "url": "http://200028503.vod.myqcloud.com/200028503_386307aa9f0d11e6820177fbfe8874c0.f30.mp4",
+    "type": 'school'
+  },
+  {
+    "id":133,
+    "title": "基础篇 第一章 工业机器人概述（1）",
+    "img": "video05.jpg",
+    "url": "http://200028503.vod.myqcloud.com/200028503_e9d4b2ce9f0b11e694a34983ad9d2cb1.f30.mp4",
+    "type": 'school'
+  }
+];
