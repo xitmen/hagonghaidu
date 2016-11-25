0c692773be9ad9fcd1498a1eb8562e73f3501488
chengyb <xitmen@163.com>
2016-11-25 10:11:20 +0800

    新增搜索

--- js/base.js
+++ js/base.js
@@ -58,9 +58,9 @@ $(function(){
 		'search': 'search_info.html'
 	};
 	var pageMapHtml = {
-			search_info: '<div class="top_title"><h2><i class="icon_back"></i>@{xh}</h2></div><div class="product"><ul><li><label>品牌</label>:@{pp}</li><li><label>型号</label>:@{xh}</li><li><label>工作空间</label>:@{gzkj}</li><li><label>有效负荷</label>:@{yxfh}</li><li><label>工作空间范围</label>:@{gzkjfw}</li><li><label>负荷范围</label>:@{fhfw}</li><li><label>系列</label>:@{xl}</li></ul></div><iframe style="width:16rem" src="http://dev.17gu.cn/html/hagonghaidu/jxsc/@{html}"></iframe>',
-			school_info: '<div class="top_title"><h2><i class="icon_back"></i>@{title}</h2></div><div class="box"><video controls><source src="@{url}" type="video/mp4">您的浏览器不支持 video 标签。</video></div>',
-			knowledge_info: '<div class="top_title"><h2><i class="icon_back"></i>@{title}</h2></div><iframe src="http://dev.17gu.cn/html/hagonghaidu/xzs/@{html}"></iframe></div>',
+			search_info: '<div class="product"><ul><li><label>品牌</label>:@{pp}</li><li><label>型号</label>:@{xh}</li><li><label>工作空间</label>:@{gzkj}</li><li><label>有效负荷</label>:@{yxfh}</li><li><label>工作空间范围</label>:@{gzkjfw}</li><li><label>负荷范围</label>:@{fhfw}</li><li><label>系列</label>:@{xl}</li></ul></div><iframe style="width:16rem" src="http://dev.17gu.cn/html/hagonghaidu/jxsc/@{html}"></iframe>',
+			school_info: '<div class="video"><video controls="controls" autoplay="autoplay"><source src="@{url}" type="video/mp4">您的浏览器不支持 video 标签。</video></div>',
+			knowledge_info: '<iframe src="http://dev.17gu.cn/html/hagonghaidu/xzs/@{html}"></iframe>',
 		};
 	var pageIndexType = {
 			school: '<dl class="school"><dt>@{title}</dt><dd><img src="data/img/@{img}" /></dd></dl>',
@@ -135,7 +135,16 @@ $(function(){
 		}
 	}
 	if( info.get(0) ){
+		var h = $(document).height();
+		var w = $(document).width();
 		var type = info.attr('type');
+		if( type == 'school' ) {
+			topNavList.eq(0).addClass('on');
+		}else if( type == 'knowledge_info' ){
+			topNavList.eq(1).addClass('on');
+		}else if( type == 'search_info' ){
+			topNavList.eq(2).addClass('on');
+		}
 		var nId = window.name;
 		var htmlMode = pageMapHtml[ type ];
 		var DATA = infoData[ type ];
@@ -158,6 +167,7 @@ $(function(){
 		}
 		return false;
 	});
+
 	oBody.on('click',function(){
 		navLeft.removeClass('open').addClass('close');
 	});
@@ -166,138 +176,4 @@ $(function(){
 		history.go(-1);
 	});
 
-
-	(function(){
-		var w = document.body.clientWidth;
-		$('.banner').each(function(){
-			var _this = $(this),
-				listBox = _this.find('.banner_img'),
-				pageInfo = _this.find('.banner_dis'),
-				imgList = listBox.find('img'),
-				len = imgList.length;
-			_this.attr('w',w).attr('num',0).attr('max',len);
-			listBox.css({width: w* len});
-			if( pageInfo.get(0) ){
-				var iList = [];
-				$.each( imgList, function( n ){
-					if( !n ){
-						iList.push('<i class="on"></i>');
-					}else{
-						iList.push('<i></i>');
-					}
-				});
-				pageInfo.html( iList.join('') );
-			}
-		});
-		loop();
-	})();
-
-	function loop(){
-		var loopBox = $(".banner");
-		var proBox = loopBox.find('.banner_img');
-		var loading = true;
-		var _startY = 0;
-		var _startX = 0;
-		var _moveX = 0;
-		var _moveY = 0;
-		var _absMoveX = 0;
-		var _absMoveY = 0;
-		var distance = 90;
-		var __isScr = true;
-		var thisLoopBox;
-		var _move = Number( loopBox.attr('w') );
-		var _index = Number( loopBox.attr('num') );
-		var _max = Number( loopBox.attr('max') );
-		var pageInfo;
-		var _pageInfo;
-		var _this;
-		// 绑定菜品区域
-		proBox.on('touchstart',function( e ){
-			if( loading ){
-				fnTouches( e );
-				fnTouchstart( e );
-			}
-		});
-		proBox.on('touchmove',function( e ){
-			if( loading){
-				fnTouches( e );
-				fnTouchmove( e );
-			}
-		});
-		proBox.on('touchend',function(  ){
-			_this = $(this);
-			if( loading ){
-				fnTouchend( $( this ) );
-			}
-		});
-		// touches
-		function fnTouches( e ){
-			if(!e.touches){
-				e.touches = e.originalEvent.touches;
-			}
-		}
-		// touchstart
-		function fnTouchstart( e ){
-			_startX = e.touches[0].pageX;
-			_startY = e.touches[0].pageY;
-		}
-		// touchmove
-		function fnTouchmove( e ){
-			_curX = e.touches[0].pageX;
-			_curY = e.touches[0].pageY;
-			_moveX = _curX - _startX;
-			_moveY = _curY - _startY;
-		}
-		// touchend
-		function fnTouchend( _this ){
-			_absMoveX = Math.abs( _moveX );
-			thisLoopBox = _this.parent('.banner');
-			_move = Number( thisLoopBox.attr('w') );
-			_index = Number( thisLoopBox.attr('num') );
-			_max = Number( thisLoopBox.attr('max') );
-			_pageInfo = thisLoopBox.find('.banner_dis i');
-			pageInfo = thisLoopBox.find('.banner_dis');
-			if( __isScr && _absMoveX > distance ){
-				if( _moveX < 0 ){
-					move( 'l', _this );
-				}else{
-					move( 'r', _this );
-				}
-				loading = false;
-			}
-			_startY = 0;
-			_startX = 0;
-			_moveX = 0;
-			_moveY = 0;
-			_absMoveX = 0;
-			_absMoveY = 0;
-		}
-		function move( type, _this ){
-			if( type === 'l' ){
-				if( _index > _max-2 ){
-					_index = _max-1;
-				}else{
-					_index++;
-				}
-				moveValue = -( _index *_move );
-			}else{
-				if( _index < 1 ){
-					_index = 0;
-				}else{
-					_index--;
-				}
-				moveValue = -(_index * _move);
-			}
-			_this.animate({
-				left: moveValue
-			}, 500,function(){
-				loading = true;
-				if( pageInfo.get(0) ){
-					_pageInfo.removeClass('on');
-					_pageInfo.eq(_index).addClass('on');
-				}
-				thisLoopBox.attr( 'num' , _index );
-			} );
-		}
-	}
 })
